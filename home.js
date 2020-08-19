3ee23a01bb2df5d386dab9eebe169d8488f8f996
John Zeichman <jzeichman@gmail.com>
2020-08-19 11:46:42 -0400

    work on side menu

--- home.js
+++ home.js
@@ -1,10 +1,22 @@
 const hamburgerIcon = document.querySelector(".hamburgerIcon");
-console.log(hamburgerIcon);
+
 hamburgerIcon.onclick = function () {
   sidebarNav.style.transform = "translateX(250px)";
+  sidebarNav.style.transition = "ease, .75s";
 };
 const closeSideBar = document.querySelector(".closeSideBar");
+
 closeSideBar.onclick = function () {
+  sidebarNav.style.transition = "ease, .75s";
   sidebarNav.style.transform = "translateX(-250px)";
 };
 const sidebarNav = document.querySelector(".sidebar");
+
+//function to close the sidebar menu when clicking on nav items, otherwise menu stays open when navigating back to homepage.
+
+function closeSide() {
+  sidebarNav.style.transition = "all, 1.5s";
+  sidebarNav.style.transform = "translateX(-250px)";
+}
+const navOptions = document.querySelectorAll(".navOption");
+navOptions.onclick = closeSide();

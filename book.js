2f1d085503466b52da9ac60200fa09b39a31bc6b
John Zeichman <jzeichman@gmail.com>
2020-08-20 12:40:43 -0400

    random updates

--- book.js
+++ book.js
@@ -2,20 +2,47 @@ const firstBook = document.querySelector(".firstBook");
 
 const secondBook = document.querySelector(".secondBook");
 
+const thirdBook = document.querySelector(".thirdBook");
+
 const firstPrevBtn = document.querySelector(".firstPrevBtn");
 
 const firstNextBtn = document.querySelector(".firstNextBtn");
 
+const secondPrevBtn = document.querySelector(".secondPrevBtn");
+
 const secondNextBtn = document.querySelector(".secondNextBtn");
+
+const thirdPrevBtn = document.querySelector(".thirdPrevBtn");
+
+const thirdNextBtn = document.querySelector(".thirdNextBtn");
 
 let reset = 1;
 let demonCounter = 1129;
 let pinchedCounter = 1059;
+let deathCounter = 988;
 
 firstNextBtn.addEventListener("click", function () {
   function turnPage() {
     let animateTurning = setInterval(function () {
       demonCounter++;
+      reset++;
+      console.log(demonCounter);
+      firstBook.style.backgroundImage = `url('bookWebIMG/demon/IMG_${demonCounter}.jpg')`;
+
+      if (reset === 5) {
+        console.log("buns");
+        clearInterval(animateTurning);
+      }
+    }, 150);
+    reset = 0;
+  }
+  turnPage();
+});
+
+firstPrevBtn.addEventListener("click", function () {
+  function turnPage() {
+    let animateTurning = setInterval(function () {
+      demonCounter--;
       reset++;
       console.log(demonCounter);
       firstBook.style.backgroundImage = `url('bookWebIMG/demon/IMG_${demonCounter}.jpg')`;
@@ -48,25 +75,20 @@ secondNextBtn.addEventListener("click", function () {
   turnPage();
 });
 
-// this is first version of next button(works)
-// firstNextBtn.addEventListener("click", turnPage);
-// // should try putting the function within the eventListerner
-// let reset = 1;
-// let counter = 1129;
+thirdNextBtn.addEventListener("click", function () {
+  function turnPage() {
+    let animateTurning = setInterval(function () {
+      deathCounter++;
+      reset++;
+      console.log(deathCounter);
+      thirdBook.style.backgroundImage = `url('bookWebIMG/death/IMG_${deathCounter}.jpg')`;
 
-// function turnPage() {
-//   let animateTurning = setInterval(function () {
-//     //if i put counter here it breaks the code
-//     //try putting counter in the eventListener
-//     counter++;
-//     reset++;
-//     console.log(counter);
-//     firstBook.style.backgroundImage = `url('bookWebIMG/demon/IMG_${counter}.jpg')`;
-
-//     if (reset === 5) {
-//       console.log("buns");
-//       clearInterval(animateTurning);
-//     }
-//   }, 150);
-//   reset = 0;
-// }
+      if (reset === 5) {
+        console.log("buns");
+        clearInterval(animateTurning);
+      }
+    }, 150);
+    reset = 0;
+  }
+  turnPage();
+});
